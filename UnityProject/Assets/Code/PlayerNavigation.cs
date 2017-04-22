2b1172105947809cc904f104d2bc0102f56e6c28
TurkeyPuncher <glen.r.chang@gmail.com>
2017-04-22 15:19:29 -0700

    - adding face camera so character avatars face Player - adding navigation with asdw for better controll - added dampening

--- UnityProject/Assets/Code/PlayerNavigation.cs
+++ UnityProject/Assets/Code/PlayerNavigation.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class PlayerNavigation : MonoBehaviour
 {
-    // Rigidbody
     [SerializeField]
     float m_speedScaleX = 60;
     [SerializeField]
-    float m_speedScaleZ = 100;
-    
+    float m_speedScaleY = 100;
+    [SerializeField]
+    float m_speedScaleZ = 60;
+    [SerializeField]
+    float m_dampen = 0.9f;
+
     // Finger touch
     Vector3 m_fingerPos = Vector3.zero;
     Vector3 m_fingerMovedBy = Vector3.zero;
 
     float m_slideMagnitudeX = 0.0f;
-    float m_slideMagnitudeZ = 0.0f;
+    float m_slideMagnitudeY = 0.0f;
+
+    // movement
+    float m_rotationX = 0f; 
+    float m_rotationY = 0f;
+    float m_rotationZ = 0f;
 
     void Update()
     {
@@ -24,8 +32,10 @@ public class PlayerNavigation : MonoBehaviour
         {
             m_fingerPos = Input.mousePosition;
             m_slideMagnitudeX = 0f;
-            m_slideMagnitudeZ = 0f;
+            m_slideMagnitudeY = 0f;
         }
+
+        // Calculate the movement by mouse drag
         if (Input.GetMouseButton(0))
         {
             // The delta since last position.
@@ -33,18 +43,37 @@ public class PlayerNavigation : MonoBehaviour
 
             // slide horizontal
             m_slideMagnitudeX = m_fingerMovedBy.x / Screen.height;
+            m_rotationX = m_slideMagnitudeX* m_speedScaleX;
 
             // slide vertical
-            m_slideMagnitudeZ = m_fingerMovedBy.y / Screen.width;
+            m_slideMagnitudeY = m_fingerMovedBy.y / Screen.width;
+            m_rotationY = m_slideMagnitudeY* m_speedScaleY;
 
             m_fingerPos = Input.mousePosition;
         }
 
-        // movement
-        float rotationX = m_slideMagnitudeX * m_speedScaleX;
-        float rotationY = m_slideMagnitudeZ * m_speedScaleZ;
-        transform.RotateAround(Vector3.zero, Camera.main.transform.up, rotationX);
-        transform.RotateAround(Vector3.zero, Camera.main.transform.right, -rotationY);
+        // Calculate movement using asdw keys
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if (horizontalMovement != 0)
+        {
+            m_rotationZ = horizontalMovement;
+        }
+
+        float verticalMovement = Input.GetAxis("Vertical");
+        if (verticalMovement != 0)
+        {
+            m_rotationY = -verticalMovement;
+        }
+
+
+        transform.RotateAround(Vector3.zero, Camera.main.transform.up, m_rotationX);
+        transform.RotateAround(Vector3.zero, Camera.main.transform.right, -m_rotationY);
+        transform.RotateAround(Vector3.zero, Camera.main.transform.forward, -m_rotationZ);
+
+        // Dampeners
+        m_rotationX *= m_dampen;
+        m_rotationY *= m_dampen;
+        m_rotationZ *= m_dampen;
     }
 }
 
